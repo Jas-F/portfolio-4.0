51e1f581513bd98bc88f3ac2f46a3bcc784dff25
Jas-F <jasminer.franklin@gmail.com>
2021-03-02 00:34:24 -0500

    add react routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+// remove portfolio folder to deploy
 import './App.css';
+import{Route, Router} from "react-router-dom";
+import Projects from "./components/projects";
 // import Projects from './components/projects';
 import Home from './components/home.js';
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Route path="/">
       <Home />
+      </Route>
+      {/* add router that requires exact path to projects */}
+      <Route exact path= "/projects">
+        <Projects/>
+      </Route>
+      </Router>
+    
     </div>
   );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './projects.css'; 
 // import components from other packages
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Container, Row, Col} from 'react-bootstrap';
 
 function projects() {
   return (
@@ -9,7 +9,7 @@ function projects() {
     <Navbar bg="primary" variant="dark">
     <Navbar.Brand href="home.js">Navbar</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="project.js">Projects</Nav.Link>
+      <Nav.Link>Projects</Nav.Link>
     </Nav>
     
   </Navbar>
