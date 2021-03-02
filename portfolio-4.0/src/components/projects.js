149ab6e36a01bcfd3b0d4ba4b8f0424ac7869c8d
Jas-F <jasminer.franklin@gmail.com>
2021-03-01 22:20:24 -0500

    add project 1

--- portfolio-4.0/src/components/projects.js
+++ portfolio-4.0/src/components/projects.js
@@ -5,6 +5,7 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 function projects() {
   return (
+    <div>
     <Navbar bg="primary" variant="dark">
     <Navbar.Brand href="home.js">Navbar</Navbar.Brand>
     <Nav className="mr-auto">
@@ -14,6 +15,22 @@ function projects() {
     
   </Navbar>
   
+  <Container>
+  <Row>
+      <Col sm={6}> <h1>Jasmine Franklin</h1>
+      <h2 className="project-title">Team Profile Generator</h2>
+        <a className="media" href="https://jas-f.github.io/team-profile-generator/">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/team-profile-generator">Github Repo</a>
+      </Col>
+      <Col sm={6}>
+        <img className="project-img" src='team.png' alt='project1'></img>
+      </Col>
+  </Row>
+
+</Container>
+</div>
+
+
 
   )
 }
