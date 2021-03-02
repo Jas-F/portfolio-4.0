9a911b957bb4590f2fdd46c618c9fbbe349b1255
Jas-F <jasminer.franklin@gmail.com>
2021-03-02 11:03:24 -0500

    add target

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,8 +4,7 @@ import './projects.css';
 import portfolioSite from "../Assets/3.0.png";
 import book from "../Assets/booklist.png";
 import generator from "../Assets/generator.png";
-import live from "../Assets/liveSite.png";
-import profile from "../Assets/profile.png";
+import live from "../Assets/live.png";
 import resp from "../Assets/responsive.png";
 import team from "../Assets/team.png";
 
@@ -29,18 +28,18 @@ function Projects() {
   <Row>
       <Col sm={6}> 
       <h2 className="project-title">Team Profile Generator</h2>
-        <a className="media" href="https://jas-f.github.io/team-profile-generator/">Deployed Link</a>
-        <a className="media" href="https://github.com/Jas-F/team-profile-generator">Github Repo</a>
+        <a className="media" href="https://jas-f.github.io/team-profile-generator/" target="_blank">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/team-profile-generator" target="_blank">Github Repo</a>
       </Col>
       <Col sm={6}>
-        <img className="project-img" src='team.png' alt='project1'></img>
+        <img className="project-img" src={team} alt='project1'></img>
       </Col>
   </Row>
   <Row>
       <Col sm={6}> 
       <h2 className="project-title">Portfolio 3.0</h2>
-        <a className="media" href="https://jas-f.github.io/portfolio-3.0/">Deployed Link</a>
-        <a className="media" href="https://github.com/Jas-F/portfolio-3.0">Github Repo</a>
+        <a className="media" href="https://jas-f.github.io/portfolio-3.0/" target="_blank">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/portfolio-3.0" target="_blank">Github Repo</a>
       </Col>
       <Col sm={6}>
         <img className="project-img" src={portfolioSite} alt='project1'></img>
@@ -49,41 +48,41 @@ function Projects() {
   <Row>
       <Col sm={6}> 
       <h2 className="project-title">Responsive Portfolio</h2>
-        <a className="media" href="https://jas-f.github.io/responsive-portfolio/">Deployed Link</a>
-        <a className="media" href="https://github.com/Jas-F/responsive-portfolio">Github Repo</a>
+        <a className="media" href="https://jas-f.github.io/responsive-portfolio/" target="_blank">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/responsive-portfolio" target="_blank">Github Repo</a>
       </Col>
       <Col sm={6}>
-        <img className="project-img" src='responsive.png' alt='project1'></img>
+        <img className="project-img" src={resp} alt='project1'></img>
       </Col>
   </Row>
   <Row>
       <Col sm={6}> 
       <h2 className="project-title">Password Generator</h2>
-        <a className="media" href="https://jas-f.github.io/team-profile-generator/">Deployed Link</a>
-        <a className="media" href="https://github.com/Jas-F/team-profile-generator">Github Repo</a>
+        <a className="media" href="https://jas-f.github.io/team-profile-generator/" target="_blank">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/team-profile-generator" target="_blank">Github Repo</a>
       </Col>
       <Col sm={6}>
-        <img className="project-img" src='generator.png' alt='project1'></img>
+        <img className="project-img" src={generator} alt='project1'></img>
       </Col>
   </Row>
   <Row>
       <Col sm={6}> 
       <h2 className="project-title">My Book List</h2>
-        <a className="media" href="https://my-book-list1.herokuapp.com/">Deployed Link</a>
-        <a className="media" href="https://github.com/Jas-F/my-book-list">Github Repo</a>
+        <a className="media" href="https://my-book-list1.herokuapp.com/" target="_blank">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/my-book-list" target="_blank">Github Repo</a>
       </Col>
       <Col sm={6}>
-        <img className="project-img" src='booklist.png' alt='project1'></img>
+        <img className="project-img" src={book} alt='project1'></img>
       </Col>
   </Row>
   <Row>
       <Col sm={6}> 
       <h2 className="project-title">Third Party Scheduler</h2>
-        <a className="media" href="https://jas-f.github.io/third-party-api-scheduler/">Deployed Link</a>
-        <a className="media" href="https://github.com/Jas-F/third-party-api-scheduler">Github Repo</a>
+        <a className="media" href="https://jas-f.github.io/third-party-api-scheduler/" target="_blank">Deployed Link</a>
+        <a className="media" href="https://github.com/Jas-F/third-party-api-scheduler" target="_blank">Github Repo</a>
       </Col>
       <Col sm={6}>
-        <img className="project-img" src='liveSite.png' alt='project1'></img>
+        <img className="project-img" src={live} alt='project1'></img>
       </Col>
   </Row>
 
